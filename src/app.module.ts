573571ab75d17c1b5df09a32ce99dc3efa87839f
Flavio Wu <flaviowu@gmail.com>
2021-12-10 01:41:29 -0300

    feat: generate prisma module & services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AirlineModule } from './airline/airline.module';
+import { FlightModule } from './flight/flight.module';
+import { SeatModule } from './seat/seat.module';
+import { PassengerModule } from './passenger/passenger.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AirlineModule, FlightModule, SeatModule, PassengerModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
