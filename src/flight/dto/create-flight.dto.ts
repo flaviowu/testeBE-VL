0d53b902adbd3ab78a5103a16fcadcc3f9824ca0
Flavio Wu <flaviowu@gmail.com>
2021-12-12 19:39:27 -0300

    feat(flight): add flight dto

--- src/flight/dto/create-flight.dto.ts
+++ src/flight/dto/create-flight.dto.ts
@@ -1,1 +1,10 @@
-export class CreateFlightDto {}
+export class CreateFlightDto {
+  flightNumber: number;
+  departure: string;
+  destination: string;
+  eta: Date;
+  etd: Date;
+  price: number;
+  airlineId: number;
+  availableSeats: number;
+}
