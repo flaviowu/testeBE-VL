1a2536e947c65d57fc4a3bd6216ca8414b1e9bb4
Flavio Wu <flaviowu@gmail.com>
2021-12-12 19:39:46 -0300

    fet(flight): add flight entity

--- src/flight/entities/flight.entity.ts
+++ src/flight/entities/flight.entity.ts
@@ -1,1 +1,17 @@
-export class Flight {}
+import { Seat } from '../../seat/entities/seat.entity';
+import { Passenger } from '../../passenger/entities/passenger.entity';
+
+export class Flight {
+  id: number;
+  createdAt: Date;
+  flightNumber: number;
+  departure: string;
+  destination: string;
+  eta: Date;
+  etd: Date;
+  price: number;
+  airlineId: number;
+  availableSeats: number;
+  Seats: Seat[];
+  Passengers: Passenger[];
+}
